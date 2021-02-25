793dae86069b54f3cf959aea373e4caf67eb28f0
miedzinskijakub <47180627+miedzinskijakub@users.noreply.github.com>
2021-02-25 20:58:50 +0100

    .

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         tablet: `@media (min-width: 768px)`,
         desktop: `@media (min-width: 1024px)`,
         huge: `@media (min-width: 1440px)`,
+        iphone: `@media(min-width: 375px)`,
         full: `@media (min-width: 1920px)`
     }
 }
